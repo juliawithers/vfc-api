b39e6107e3df362b4c15c4f9d3e680bf042b8836
Julia Fehr <09jwithers@gmail.com>
2020-05-28 22:13:46 -0400

    trying to deploy

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://postgres@localhost/vfc",
-    // TEST_DATABASE_URL= process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/vfc-test",
     CLIENT_ORIGIN: "http://localhost:3000"
 }
